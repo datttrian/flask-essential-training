c05db3bd5f07f2200462f5a77c9f8225818a2f9f
datttrian <datttrian@gmail.com>
2023-09-19 08:12:55 +0000

    Page templates in Flask with Jinja

--- url_shortener/hello.py
+++ url_shortener/hello.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello Flask!"
+    return render_template("home.html", name="Nick")
+
+
+@app.route("/about")
+def about():
+    return "This is a url shortener"
