c560bd9d87606bd568f328caf3efa27266d2b5eb
datttrian <datttrian@gmail.com>
2023-09-25 14:26:28 +0000

    File uploads from users

--- url_shortener/app.py
+++ url_shortener/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'somerandomstring'
@@ -26,7 +27,17 @@ def your_url():
             )
             return redirect(url_for('home'))
 
-        urls[request.form['code']] = {'url': request.form['url']}
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url': request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save(
+                '/workspaces/flask-essential-training/url_shortener'
+                + full_name
+            )
+            urls[request.form['code']] = {'file': full_name}
+
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
