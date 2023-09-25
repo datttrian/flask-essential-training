15067c645910e307d460e756780d88a8261fab15
datttrian <datttrian@gmail.com>
2023-09-25 13:46:03 +0000

    Update editor config

--- url_shortener/app.py
+++ url_shortener/app.py
@@ -7,24 +7,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
 
-@app.route("/your-url", methods=["GET", "POST"])
+@app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
-    if request.method == "POST":
+    if request.method == 'POST':
         urls = {}
 
-        if os.path.exists("urls.json"):
-            with open("urls.json") as urls_file:
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
                 urls = json.load(urls_file)
 
-        if request.form["code"] in urls.keys():
-            return redirect(url_for("home"))
+        if request.form['code'] in urls.keys():
+            return redirect(url_for('home'))
 
-        urls[request.form["code"]] = {"url": request.form["url"]}
-        with open("urls.json", "w") as url_file:
+        urls[request.form['code']] = {'url': request.form['url']}
+        with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
-        return render_template("your_url.html", code=request.form["code"])
+        return render_template('your_url.html', code=request.form['code'])
     else:
-        return redirect(url_for("home"))
+        return redirect(url_for('home'))
