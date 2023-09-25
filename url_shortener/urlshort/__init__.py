5658638891ac661cb5282fbd958b0a431b7cf1cd
datttrian <datttrian@gmail.com>
2023-09-25 17:04:04 +0000

    Update Blueprints and views for organization

--- url_shortener/urlshort/__init__.py
+++ url_shortener/urlshort/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app(test_config=None):  # type: ignore
     app = Flask(__name__)
     app.secret_key = 'somerandomstring'
 
